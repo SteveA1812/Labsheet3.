638d74ea2fe3dc4489769fa1a498de861680c3bc
SteveA1812 <46904949+SteveA1812@users.noreply.github.com>
2019-02-20 21:49:42 +0000

    Exercise 1

---
+++ Labsheet3.E1/Expenses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet3.E1
+{
+    class Expenses
+    {
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ClaimedDate { get; set; }
+       
+
+        public override string ToString()
+        {
+            return $"{Type} {Amount:C} on {ClaimedDate.ToShortDateString()} ";
+        }
+
+
+    }
+}
--- Labsheet3.E1/MainWindow.xaml.cs
+++ Labsheet3.E1/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Text;
 
 namespace Labsheet3.E1
 {
@@ -20,9 +10,88 @@ namespace Labsheet3.E1
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Expenses> expenses = new ObservableCollection<Expenses>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Expenses e1 = new Expenses() { Type = "Office", Amount = 15.55m, ClaimedDate = new DateTime(2014, 02, 12) };
+            Expenses e2 = new Expenses() { Type = "Entertainment", Amount = 82.63m, ClaimedDate = new DateTime(2014, 01, 25) };
+            Expenses e3 = new Expenses() { Type = "Entertainment", Amount = 26.81m, ClaimedDate = new DateTime(2014, 02, 06) };
+            expenses.Add(e1);
+            expenses.Add(e2);
+            expenses.Add(e3);
+            lbxExpenses.ItemsSource = expenses;
+            txtblkTotalExpenses.Text = "Total Expenses €" + GetTotalExpenses().ToString();
+
+        }
+
+        private void btnAddExpense_Click(object sender, RoutedEventArgs e)
+        {
+            //Create random info
+            string type = RandomString(5, true);
+            RandomGenerator generator = new RandomGenerator();
+            int rand = generator.RandomNumber(0, 100);
+            decimal amount = Convert.ToDecimal(rand);
+            DateTime claimedDate = DateTime.Now;
+
+            //create object with info
+            Expenses newExpenses = new Expenses() { Type = type, ClaimedDate = claimedDate, Amount = amount };
+
+            //add to collection
+            expenses.Add(newExpenses);
+            txtblkTotalExpenses.Text = "Total Expenses €" + GetTotalExpenses().ToString();
+        }
+        private decimal GetTotalExpenses()
+        {
+            decimal total = 0;
+
+            foreach (Expenses e in expenses)
+            {
+                total += e.Amount;
+            }
+
+            return total;
+        }
+
+        private string RandomString(int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+            return builder.ToString();
+
+        }
+    
+
+        private class RandomGenerator
+        {
+            // Generate a random number between two numbers    
+            public int RandomNumber(int min, int max)
+            {
+                Random random = new Random();
+                return random.Next(min, max);
+            }
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            expenses.RemoveAt(expenses.Count - 1);
+            txtblkTotalExpenses.Text = "Total Expenses €" + GetTotalExpenses().ToString();
+        }
     }
 }
+
+   
